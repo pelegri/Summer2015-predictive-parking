e1804d60e409cef5d58e9bcd204b4c92eb5bbe43
jaazpa3 <jaazpa3@gmail.com>
2015-06-22 17:49:53 -0700

    renamed files

--- data_collect.py
+++ data_collect.py
@@ -25,6 +25,8 @@ import boto
 from boto.s3.key import Key
 
 API_KEY = os.environ.get('VIMOC_API_KEY') #in bash_profile
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 class Parking():
 	def __init__(self):
@@ -35,13 +37,20 @@ class Parking():
 		self.zones = {}
 		self.time_by_zone = {}
 		self.latlon_by_zone = {}
+		self.site_by_zone = {}
+		self.timezone_by_zone = {}
 
 		r = requests.get('http://api.landscape-computing.com/api/rest/v1/sensor/pa_1_P1/query/occupied?key=' + API_KEY, headers={'content-type': 'application/json', 'Accept': 'application/json'})
 		self.timestamp = r.json()[0]['readingTime'].encode('utf-8')
 
+	def time_wrapper(self,zone_list):
+		for i in range(len(zone_list)):
+			self.get_time_by_zone(zone_list[i])
+
 	def get_all_zones(self):
 		"""Retrieve all zones."""
 		r = requests.get('http://api.landscape-computing.com/api/rest/v1/sites/?key=' + API_KEY, headers={'content-type': 'application/json', 'Accept': 'application/json'})
+		
 		r_json = r.json()
 
 		all_zones = []
@@ -53,6 +62,7 @@ class Parking():
 				zone = r_json[i][u'zone'][j][u'guid'].encode('utf-8')
 				all_zones.append(zone)
 				lat,lon =  r_json[i][u'zone'][j][u'gpsCoord'][0].encode('utf-8').split(',')
+				self.timezone_by_zone[zone] = r_json[i][u'timeZone']
 				self.latlon_by_zone[zone] = {'latitude': lat, 'longitude': lon.strip()}
 				j+=1
 		return all_zones
@@ -106,7 +116,7 @@ class Parking():
 
 	def zone_wrapper(self,site_list):
 		for i in range(len(site_list)):
-			a = self.get_sensors_in_zone(site_list[i])
+	 		a = self.get_sensors_in_zone(site_list[i])
 
 	def get_vacancy_by_zone(self,zone):
 		sensors = self.sensors_by_zone[zone]
@@ -137,7 +147,8 @@ class Parking():
 		self.zones = self.get_all_zones()
 		self.zone_wrapper(self.zones)
 		self.vacancy_wrapper(self.zones)
-		self.time_by_zone[self.timestamp.encode('utf-8')] = self.vacancy_by_zone
+		#self.time_wrapper(self.zones)
+		#print self.time_by_zone
 
 class Weather():
 	def __init__(self, lat, lon):
@@ -157,25 +168,35 @@ class Weather():
 
 class S3():
 	def __init__(self, content, time, zone):
-		c = boto.connect_s3()
+		c = boto.connect_s3(AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY)
 		b = c.get_bucket('parkingbucket')
 		k = Key(b)
+		sub_dir = 'files'
 		filename = time + '_' + zone + '.json'
-		with open(filename,'w+') as f:
-			f.write(content)
+		subdir = os.path.dirname(filename)
+		if not os.path.exists(sub_dir):
+			os.makedirs(sub_dir)
+		with open(os.path.join(sub_dir,filename),'w+') as f:
+		#with open(filename,'w+') as f:
+			print 'test'
+			#f.write(content)
 		k.key = filename
-		k.set_contents_from_filename(filename)
+		k.set_contents_from_filename(os.path.join(sub_dir,filename))
 
 if __name__ == '__main__':
 	p = Parking()
 	p.main()
 
-	for i in range(len(p.zones)):
-		w = Weather(p.latlon_by_zone[p.zones[i]]['latitude'],p.latlon_by_zone[p.zones[i]]['longitude'])
-		try: 
-			d = json.dumps({'time': p.timestamp, 'zone': p.zones[i], 'weather_field': w.main(), 'vacancy_field': p.vacancy_by_zone[p.zones[i]]})
-			S3(d, p.timestamp, p.zones[i])
-		except KeyError:
-			pass
-
+	def run():
+		for i in range(len(p.zones)):
+			w = Weather(p.latlon_by_zone[p.zones[i]]['latitude'],p.latlon_by_zone[p.zones[i]]['longitude'])
+			try: 
+				d = json.dumps({'time': p.timestamp, 'zone': p.zones[i], 'timezone': p.timezone_by_zone[p.zones[i]], 'weather_field': w.main(), 'vacancy_field': p.vacancy_by_zone[p.zones[i]]})
+				S3(d, p.timestamp, p.zones[i])
+				print 'done'
+			except KeyError:
+				print 'failed'
+				pass
+	run()
+	
 
